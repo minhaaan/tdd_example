e026c720594935c4c9b08886edec19ce156b886b
최민한 <chmh0411@gmail.com>
2022-06-01 03:04:18 +0900

    ㅠ

--- tdd_example/tdd_example/APIProvider.swift
+++ tdd_example/tdd_example/APIProvider.swift
@@ -1,9 +1,18 @@
 import Moya
 import Foundation
+import RxSwift
 
-class APIProvider {
+class APIProvider: ProviderProtocol {
+  typealias T = PostsApi
+  var provider: MoyaProvider<PostsApi>
   
-  let provider: MoyaProvider<PostsApi
+  required init(isStub: Bool = false, sampleStatusCode: Int = 200, customEndpointClosure: ((PostsApi) -> Endpoint)? = nil) {
+    provider = Self.consProvider(isStub, sampleStatusCode, customEndpointClosure)
+  }
+  
+  func posts() -> Single<[Post]> {
+    request(type: [Post].self, atKeyPath: "value", target: .init())
+  }
   
   
 }
--- tdd_example/tdd_example/PostModel.swift
+++ tdd_example/tdd_example/PostModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Post: Decodable {
+struct Post: Decodable, Equatable {
   let userId: Int
   let id: Int
   let title: String
